067c3b7f94709b22adcdb53af39bef91647269a2
Hanno Günther <hanno.guenther@worldiety.de>
2022-12-04 17:44:37 +0100

    tidyup day2

--- src/main/kotlin/02.kt
+++ src/main/kotlin/02.kt
@@ -1,88 +1,84 @@
 import java.io.File
 
-@Suppress("ClassName")
-object day2 {
+private sealed interface Shape {
+    val value: Int
+    infix fun battle(other: Shape): Int
+}
 
-    sealed interface Shape {
-        val value: Int
-        infix fun battle(other: Shape): Int
+private object Rock : Shape {
+    override val value: Int = 1
+    override infix fun battle(other: Shape) = when (other) {
+        Rock -> 3
+        Paper -> 0
+        Scissor -> 6
+    }
+}
+
+private object Paper : Shape {
+    override val value: Int = 2
+    override infix fun battle(other: Shape) = when (other) {
+        Rock -> 6
+        Paper -> 3
+        Scissor -> 0
+    }
+}
+
+private object Scissor : Shape {
+    override val value: Int = 3
+    override infix fun battle(other: Shape) = when (other) {
+        Rock -> 0
+        Paper -> 6
+        Scissor -> 3
+    }
+}
+
+private data class EncodedStrategy(val other: String, val mine: String)
+
+private val encodedStrategies: List<EncodedStrategy> = File("input/02.txt")
+    .readLines()
+    .map { line ->
+        val (other, mine) = line.split(' ')
+        EncodedStrategy(other, mine)
     }
 
-    object Rock : Shape {
-        override val value: Int = 1
-        override infix fun battle(other: Shape) = when (other) {
-            Rock -> 3
-            Paper -> 0
-            Scissor -> 6
-        }
+private fun pointsOfRockPaperScissorBattle(other: Shape, mine: Shape): Int {
+    return mine.value + (mine battle other)
+}
+
+fun day2() {
+    fun EncodedStrategy.otherShape(): Shape = when (other) {
+        "A" -> Rock
+        "B" -> Paper
+        "C" -> Scissor
+        else -> throw IllegalArgumentException("wrong input")
     }
 
-    object Paper : Shape {
-        override val value: Int = 2
-        override infix fun battle(other: Shape) = when (other) {
-            Rock -> 6
-            Paper -> 3
-            Scissor -> 0
-        }
+    fun EncodedStrategy.myShape(): Shape = when (mine) {
+        "X" -> Rock
+        "Y" -> Paper
+        "Z" -> Scissor
+        else -> throw IllegalArgumentException("wrong input")
     }
 
-    object Scissor : Shape {
-        override val value: Int = 3
-        override infix fun battle(other: Shape) = when (other) {
-            Rock -> 0
-            Paper -> 6
-            Scissor -> 3
-        }
+    val totalPoints = encodedStrategies.sumOf {
+        pointsOfRockPaperScissorBattle(it.otherShape(), it.myShape())
     }
+    println("total points of rock paper scissor battles: $totalPoints")
 
-    data class EncodedStrategy(val other: String, val mine: String)
 
-    private val encodedStrategies: List<EncodedStrategy> = File("input/02.txt")
-        .readLines()
-        .map { line ->
-            val (other, mine) = line.split(' ')
-            EncodedStrategy(other, mine)
-        }
-
-    class Strategy(val other: Shape, val mine: Shape) {
-        val points: Int = mine.value + (mine battle other)
-    }
-
-    operator fun invoke() {
-        fun EncodedStrategy.otherShape(): Shape = when (other) {
-            "A" -> Rock
-            "B" -> Paper
-            "C" -> Scissor
+    fun EncodedStrategy.myInferredShape(): Shape {
+        val intendedPoints = when (mine) {
+            "X" -> 0
+            "Y" -> 3
+            "Z" -> 6
             else -> throw IllegalArgumentException("wrong input")
         }
-
-        fun EncodedStrategy.myShape(): Shape = when (mine) {
-            "X" -> Rock
-            "Y" -> Paper
-            "Z" -> Scissor
-            else -> throw IllegalArgumentException("wrong input")
-        }
-
-        val totalPoints = encodedStrategies.sumOf {
-            Strategy(it.otherShape(), it.myShape()).points
-        }
-        println("total points of rock paper scissor battles: $totalPoints")
-
-
-        fun EncodedStrategy.myInferredShape(): Shape {
-            val intendedPoints =  when (mine) {
-                "X" -> 0
-                "Y" -> 3
-                "Z" -> 6
-                else -> throw IllegalArgumentException("wrong input")
-            }
-            val otherShape = otherShape()
-            return listOf(Rock, Paper, Scissor).first { it battle otherShape == intendedPoints }
-        }
-
-        val totalInferredPoint = encodedStrategies.sumOf {
-            Strategy(it.otherShape(), it.myInferredShape()).points
-        }
-        println("total points of rock paper scissor battles with intended results: $totalInferredPoint")
+        val otherShape = otherShape()
+        return listOf(Rock, Paper, Scissor).first { it battle otherShape == intendedPoints }
     }
+
+    val totalInferredPoint = encodedStrategies.sumOf {
+        pointsOfRockPaperScissorBattle(it.otherShape(), it.myInferredShape())
+    }
+    println("total points of rock paper scissor battles with intended results: $totalInferredPoint")
 }
